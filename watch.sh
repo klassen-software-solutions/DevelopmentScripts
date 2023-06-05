45ca64a50d8549b19a425868da88297b7f879702
Steven Klassen <klassens@acm.org>
2023-06-05 08:16:02 -0600

    Modified the watch command so it does not end when the underlying command fails

--- watch.sh
+++ watch.sh
@@ -3,8 +3,6 @@
 # Shell implementation of a "watch" - like command.
 # Adapted from a code found at https://stackoverflow.com/questions/9574089/os-x-bash-watch-command
 
-set -e
-
 if [ "$#" -eq 1 ]; then
     command=$1
     duration=2
